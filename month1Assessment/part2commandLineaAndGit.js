405b2f4b998066bfccdc5abce689b3185660162f
starrpatty28 <starrpatty28@gmail.com>
2019-08-02 09:50:00 -0700

    Completed Command Line and Git 1 Month Assessment

---
+++ month1Assessment/part2commandLineaAndGit.js
@@ -0,0 +1,51 @@
+// Welcome to the Month 1 assessment, Part 2: Command line and git/GitHub
+// For this section, please DO NOT Google or use any references.
+// If you can't fully complete a question, please write down your best guess or your thoughts so far.
+// Recommended time: 10 minutes
+
+// Command Line Questions
+
+// What’s the command to switch to another directory?
+
+When I am in a directory I always cd ../ until I am where I want to be.For  example to get to my desktop I 'cd Desktop' to get there
+
+// What’s the command to switch to the parent directory of the one you’re currently in?
+
+I am taking a guess: ' /~' or '~'/'   ;
+
+//What’s the command to list all files and directories in your current directory?
+
+ls or -ls
+// What’s the command to see what directory you’re currently in?
+
+pwd
+
+// Choose one of these commands and explain what it does:
+// man
+// mkdir 
+this makes a new directory. I use it most to make folders
+// cp
+I honestly dont remember using this
+// mv
+I use this to move files to other files
+// grep
+I do not remember using this but my guess would be 'get repo' because I do not use this DOMMatrixReadOnly, I cant remember right now. 
+
+
+// GitHub questions
+
+// What does it mean to clone a repository?
+
+When I am cloning a respository, I am downloading a file from that repository. For example: If I am working on a website and it has a HTML, CSS and JS file, When someone clones that repository they will download all the files pertaining to that respository.
+
+// What is a pull request?
+
+I cant remeber for sure by my guess would be to request to pull someone respository.
+
+// What are some benefits of using version control (e.g. git and GitHub)?
+
+Pros for git
+ Personally I like Git because  I can do everything from the terminal using Git. I can create a new document (touch), I can create a new folder (mkdir), I can move a file (mv), I can remove a file (rm) all from using Git.
+ 
+ Pros for GitHub
+ What I like about GitHub is I am able to push projects I work on to the cloud through Git. When I work on a file like this onemptied, when done I will push to GitHub and someone can read my files. Someone can also download this file and give feedback and then I download it and read the feedback. Also, as a developer you do not necessarly have to always have frontend work so being able to show your coding work, GitHub is a great place to show your thought process on why you coded what you coded. 
