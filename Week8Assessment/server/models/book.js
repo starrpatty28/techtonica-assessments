ba99c172e320a2bd3a485167887cbfea787abbe2
starrpatty28 <starrpatty28@gmail.com>
2019-08-26 10:53:58 -0700

    Created a book model file book.js and updated the file to use ES6

--- Week8Assessment/server/models/book.js
+++ Week8Assessment/server/models/book.js
@@ -1,13 +1,43 @@
-'use strict';
-module.exports = (sequelize, DataTypes) => {
+export default (sequelize, DataTypes) => {
   const Book = sequelize.define('Book', {
-    title: DataTypes.STRING,
-    author: DataTypes.STRING,
-    description: DataTypes.STRING,
-    quantity: DataTypes.INTEGER,
-    userId: DataTypes.INTEGER
+    title: {
+      type: DataTypes.STRING,
+      allowNull: {
+        args: false,
+        msg: 'Please enter the title for your book'
+      }
+    },
+    author: {
+      type: DataTypes.STRING,
+      allowNull: {
+        args: false,
+        msg: 'Please enter an author'
+      }
+    },
+    description: {
+      type: DataTypes.STRING,
+      allowNull: {
+        args: false,
+        msg: 'Pease input a description'
+      }
+    },
+    quantity: {
+      type: DataTypes.INTEGER,
+      allowNull: {
+        args: false,
+        msg: 'Pease input a quantity'
+      }
+    },
+    userId: {
+      type: DataTypes.INTEGER,
+      references: {
+        model: 'User',
+        key: 'id',
+        as: 'userId',
+      }
+    }
   }, {});
-  Book.associate = function(models) {
+  Book.associate = (models) => {
     // associations can be defined here
   };
   return Book;
