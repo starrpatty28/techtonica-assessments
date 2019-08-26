ece2bf22e3ed22b84bf66345427bdf8a22a0eab2
starrpatty28 <starrpatty28@gmail.com>
2019-08-26 16:24:15 -0700

    Update the create-user migration file

--- week8assessment/server/migrations/20190826231933-create-user.js
+++ week8assessment/server/migrations/20190826231933-create-user.js
@@ -1,36 +1,38 @@
-'use strict';
 module.exports = {
   up: (queryInterface, Sequelize) => {
     return queryInterface.createTable('Users', {
-      id: {
-        allowNull: false,
-        autoIncrement: true,
-        primaryKey: true,
-        type: Sequelize.INTEGER
-      },
-      name: {
-        type: Sequelize.STRING
-      },
-      username: {
-        type: Sequelize.STRING
-      },
-      email: {
-        type: Sequelize.STRING
-      },
-      password: {
-        type: Sequelize.STRING
-      },
-      createdAt: {
-        allowNull: false,
-        type: Sequelize.DATE
-      },
-      updatedAt: {
-        allowNull: false,
-        type: Sequelize.DATE
-      }
+        id: {
+          allowNull: false,
+          autoIncrement: true,
+          primaryKey: true,
+          type: Sequelize.INTEGER
+        },
+        name: {
+          allowNull: false,
+          type: Sequelize.STRING
+        },
+        username: {
+          allowNull: false,
+          type: Sequelize.STRING
+        },
+        email: {
+          allowNull: false,
+          unique: true,
+          type: Sequelize.STRING
+        },
+        password: {
+          type: Sequelize.STRING
+        },
+        createdAt: {
+          allowNull: false,
+          type: Sequelize.DATE
+        },
+        updatedAt: {
+          allowNull: false,
+          type: Sequelize.DATE
+        }
     });
   },
-  down: (queryInterface, Sequelize) => {
-    return queryInterface.dropTable('Users');
-  }
+  down: queryInterface /* , Sequelize */ => queryInterface.dropTable('Users')
 };
+
