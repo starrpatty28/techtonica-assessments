2d0c835b2562eced37792c8c7acb8ebdad2f88aa
starrpatty28 <starrpatty28@gmail.com>
2019-08-26 18:35:42 -0700

    Created new routes for Put and Post

---
+++ week8assessment/server/controllers/book.js
@@ -0,0 +1,55 @@
+import model from "../models";
+
+const { Book } = model;
+
+class Books {
+  static modify(req, res) {
+    const { title, author, description, quantity } = req.body
+    return Book
+      .findByPk(req.params.bookId)
+      .then((book) => {
+        book.update({
+          title: title || book.title,
+          author: author || book.author,
+          description: description || book.description,
+          quantity: quantity || book.quantity
+        })
+        .then((updatedBook) => {
+          res.status(200).send({
+            message: 'Book updated successfully',
+            data: {
+              title: title || updatedBook.title,
+              author: author || updatedBook.author,
+              description: description || updatedBook.description,
+              quantity: quantity || updatedBook.quantity
+            }
+          })
+        })
+        .catch(error => res.status(400).send(error));
+      })
+      .catch(error => res.status(400).send(error));
+  }
+  static list(req, res) {
+    return Book
+      .findAll()
+      .then(books => res.status(200).send(books));
+  } 
+  static create(req, res) {
+    const { title, author, description, quantity } = req.body;
+    const { userId } = req.params;
+    return Book.create({
+      title,
+      author,
+      description,
+      quantity,
+      userId
+    }).then(book =>
+      res.status(201).send({
+        message: `Your book with the title ${title} has been created successfully `,
+        book
+      })
+    );
+  }
+}
+
+export default Books;
--- week8assessment/server/routes/index.js
+++ week8assessment/server/routes/index.js
@@ -1,12 +1,15 @@
-import Users from '../controllers/user';
+import Users from "../controllers/user";
+import Books from "../controllers/book";
 
-        export default (app) => {
+export default app => {
+  app.get("/api", (req, res) =>
+    res.status(200).send({
+      message: "Welcome to the bookStore API!"
+    })
+  );
+  app.get('/api/books', Books.list); // API route for user to get all books in the database 
+  app.post("/api/users", Users.signUp); // API route for user to signup
+  app.post("/api/users/:userId/books", Books.create); // API route for user to create a book
+  app.put('/api/books/:bookId', Books.modify); // API route for user to edit a book
 
-          app.get('/api', (req, res) => res.status(200).send({
-            message: 'Welcome to the BookStore API!',
-          }));
-
-          app.post('/api/users', Users.signUp); // API route for user to signup
-        }; 
-
-        
+};
