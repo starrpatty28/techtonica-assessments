c2a8dcbbd7bc457cd0198dd5b35aabf8f702b7ae
starrpatty28 <starrpatty28@gmail.com>
2019-08-26 17:33:54 -0700

    Need to make the application aware of the route file we just created

--- week8assessment/app.js
+++ week8assessment/app.js
@@ -1,24 +1,25 @@
-import http from 'http';
-    import express from 'express';
-    import logger from 'morgan';
-    import bodyParser from 'body-parser';
+        import http from 'http'
+        import express from 'express'
 
-    const hostname = '127.0.0.1';
-    const port = 3000;
-    const app = express() // setup express application
-    const server = http.createServer(app);
+        import logger from 'morgan';
+        import bodyParser from 'body-parser';
+        import routes from './server/routes';
 
-    app.use(logger('dev')); // log requests to the console
+        const hostname = '127.0.0.1';
+        const port = 3000;
+        const app = express()
+        const server = http.createServer(app);
 
-    // Parse incoming requests data
-    app.use(bodyParser.json());
-    app.use(bodyParser.urlencoded({ extended: false }));
+        app.use(logger('dev'));
+        app.use(bodyParser.json());
+        app.use(bodyParser.urlencoded({ extended: false }));
 
-    app.get('*', (req, res) => res.status(200).send({
-      message: 'Welcome to the default API route',
-    }));
+        routes(app);
 
-    server.listen(port, hostname, () => {
-      console.log(`Server running at http://${hostname}:${port}/`);
-    });
-    
+        app.get('*', (req, res) => res.status(200).send({
+          message: 'Welcome to the .',
+        }));
+
+        server.listen(port, hostname, () => {
+          console.log(`Server running at http://${hostname}:${port}/`);
+        });
