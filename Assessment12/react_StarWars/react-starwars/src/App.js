6d476ddb7ddefcd04b4bece6f32c6da813b6a410
starrpatty28 <starrpatty28@gmail.com>
2019-09-25 17:20:04 -0700

    Finished Project

---
+++ Assessment12/react_StarWars/react-starwars/src/App.js
@@ -0,0 +1,53 @@
+import React, { Component } from "react";
+import axios from "axios";
+
+
+import './App.css';
+
+class App extends Component {
+  constructor() {
+    super();
+    this.state = {
+      planet: null
+    }
+    this.onClick = this.onclick.bind(this);
+  }
+
+
+onclick() {
+  const num = this.getRandomNumber();
+  this.getPlanet(num)
+}
+
+getRandomNumber() {
+  return Math.floor(Math.random() * 60) + 1;
+}
+
+getPlanet(num) {
+  const url = `https://swapi.co/api/planets/${num}`
+  console.log(url);
+
+  axios.get(url)
+  .then(response => {
+    this.setState({
+      planet: response.data.name
+      });
+    });
+  } 
+
+  render() {
+    return (
+      <div className="App">
+        <img src="https://media.giphy.com/media/3o6gaUYip73ooXZWEw/giphy.gif" 
+        alt="Italian Trulli"></img>
+        <h1>{this.state.planet}</h1>
+        <button type="button" onClick={this.onClick}>
+          Click Me!
+          </button>
+      </div>
+    );
+  }
+  
+}
+
+export default App;
