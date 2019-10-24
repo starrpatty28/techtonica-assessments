6291b7f360c22eb96ff7755fa76035d18595a9c3
starrpatty28 <starrpatty28@gmail.com>
2019-10-24 10:00:54 -0700

    3rd Final Assessment...so they say

---
+++ 3rdFinalAssessment/3rdFinalAssessment.js
@@ -0,0 +1,66 @@
+// function primeNum(int){
+//   // if(typeof int !== Integer){
+//   //   return "Please enter an Integer";
+//   // }
+//   if(int % 3 === 0) {
+//     return false
+//   }
+//   else {
+//     return true;
+//   }
+// }
+// console.log(primeNum(7));
+// console.log(primeNum(15));
+//console.log(primeNum("string"));
+
+//Function takes in a Int
+//Function spit out a boolean
+
+//I wanted to check if the number given was an integer and not a sting....will work on that later
+//Create an if/else statement asking if the number is modelous with a 0 remainder. This was my thought process
+//I beleive this function has a run time of O(N)
+
+// function recursionFunc(int) {
+//     if(int % 2 === 0) {
+//       return false;
+//     }else {
+//       return true;
+//     }
+// }
+// recursionFunc(6);
+//recursionFunc(12);
+
+//Function takes in an Int
+//Function spits out an array
+
+//I beleieve we had this same question before but the way my head is right now, I am unable to solev this with the right syntax
+//my thought process was this was a recursive function if they reccomend that the first function could be used
+//My game plan would be to create an if/esle statement to check is the number is a prime with a modulas 
+//if the number is a prime, figure out a way to count through that number and if the the number is a prime, print out that number in an array. 
+
+//I am clearly having problems with my debugger as I am unable to run anyting on this problem
+
+// function checkArr(array){
+//   // if(typeof array !== "Array") {
+//   //   return "Please enter a Array";
+//   // }
+//   for(i = 0; i < array.length; i++) {
+//     //console.log(i);
+//     //console.log(array.beginsWith);
+//     if(array.beginsWith === "S") {
+//       return true;
+//     }
+//     else return false;
+//   }
+// }
+// console.log(checkArr(["bat", "sat", "superb", "ship", "bell"]))//True
+//console.log(checkArr(["ax", "art", "radio", "frog"]))  //False
+
+//Function takes in an array
+//Function spits out an Boolean
+
+//Game Plan
+//Loop through the array and console.log to make sure the loops runs through correctly
+//Figure out a way to check the first letters of each item array[i] and if they match true
+//Or not false
+  //I remeber there is a way to check the first item and of course I cannot remeber so I will work this out...
