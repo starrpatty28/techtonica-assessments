001135803a30779322d7f5628a16b1abea86194f
starrpatty28 <starrpatty28@gmail.com>
2019-11-15 10:50:44 -0800

    Finished the second part of the question 1

--- 3rdFinalAssessment/3rdFinalAssessment.js
+++ 3rdFinalAssessment/3rdFinalAssessment.js
@@ -22,25 +22,31 @@ console.log(primeNum(15));
 //else true 
 //I beleive this function has a run time of O(N)
 
-// function recursionFunc(int) {
-//     if(int % 2 === 0) {
-//       return false;
-//     }else {
-//       return true;
-//     }
-// }
-// recursionFunc(6);
-//recursionFunc(12);
+//write a function that takes an input and returns an array of all prime numbers between 2 and the input
+
+function primeDisplay(int) {
+  var array = [2];
+  for ( let i = 3; i < int; i += 2) {
+      if (primeNum(i)) {
+          array.push(i);
+      }
+  }
+  console.log(array);
+}
+console.log(primeDisplay(6));
+console.log(primeDisplay(12));
 
 //Function takes in an Int
 //Function spits out an array
 
-//I beleieve we had this same question before but the way my head is right now, I am unable to solev this with the right syntax
-//my thought process was this was a recursive function if they reccomend that the first function could be used
-//My game plan would be to create an if/esle statement to check is the number is a prime with a modulas 
-//if the number is a prime, figure out a way to count through that number and if the the number is a prime, print out that number in an array. 
+//Game Plan
+//create an empty variable that starts at 2
+//create a for loop where i starts at 3 
+//let the loop run through the prime numbers until the i is less than the int
+//if the primeNum[i]
+//push that number to the array
+//every array will start with 2 and the rest of the prime numbers 
 
-//I am clearly having problems with my debugger as I am unable to run anyting on this problem
 
 function checkArr(array){ 
   for(i = 0; i < array.length; i++) {
@@ -63,5 +69,5 @@ function checkArr(array){
   //console.log array[i].charAt(0) the first letters
 //Check to see if the first letter  is the same of the first second letter and the same as the 3rd first letter, if it is, then retrun true
 //else false
-  //It passes the first test. I am missing something but I do not know what.O(N) 
+  //It passes the first test. I am missing something but I do not know what O(N) 
 //Bad anxiety and the proctors are talking and laughing during the test, unacceptable and irritating.
